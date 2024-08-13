b81d1c026b3d57a543e5e0324242a6f225da244a
Luca Vaccaro <me@lvaccaro.com>
2024-08-13 18:33:30 +0200

    Initial Commit

---
+++ LWKDemo/LWKDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LWKDemoApp.swift
+//  LWKDemo
+//
+//  Created by luca on 13/08/24.
+//
+
+import SwiftUI
+
+@main
+struct LWKDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
